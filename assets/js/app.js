7cb9ab3b24203d6e49f38f5cff7d1fb535d3df1f
Maria <mariacutipa.gonzales@gmail.com>
2017-03-06 18:35:43 -0500

    todo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,27 @@
+var coders = [ { nombre : "Meche", apellido : "Zubieta",  promocion : "1era. promoción", estado : "Trabajando" },
+			 { nombre : "Arabela", apellido : "Rojas", promocion : "2da. promoción", estado : "Trabajando" },
+			 { nombre : "Maria", apellido : "Rosán",  promocion : "2da. promoción", estado : "Trabajando" },
+			 { nombre : "Guadalupe", apellido : "Racio",  promocion : "3era. promoción", estado : "Trabajando" },
+			 { nombre : "Sara", apellido : "Casa",  promocion : "3era. promoción", estado : "Trabajando" },
+			 { nombre : "Daniella", apellido : "Durán",  promocion : "4ta. promoción", estado : "Trabajando" },
+			 { nombre : "Michelle", apellido : "Seguil",  promocion : "5ta. promoción", estado : "Trabajando" },
+			 { nombre : "Diana", apellido : "Navarro",  promocion : "5ta. promoción", estado : "Trabajando" } ];
+
+
+
+function listarCoders(){
+	for (var i=0; i<coders.length;i++){
+		document.getElementsByClassName("div-coder")[i].innerHTML +="Nombre: "+coders[i].nombre+"<br>"+
+							    "Apellido: "+coders[i].apellido+"<br>"+
+							   	"Promoción: "+coders[i].promocion+"<br>"+
+							    "Estado: "+coders[i].estado;	 
+	}	
+}listarCoders();
+
+function redirect(){
+	window.location.href = "index.html";
+}
+
 /*DROPDOWN JS*/
 var button = document.getElementsByClassName('sprint');
 
